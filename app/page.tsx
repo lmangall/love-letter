62e518e7ca8d5a3742f62c4cf65f8e09a061aea0
lmangall <l.mangallon@gmail.com>
2024-02-25 20:54:26 +0100

    box tweaking

--- app/page.tsx
+++ app/page.tsx
@@ -40,48 +40,49 @@ export default function Home() {
         objectFit="cover"
         alt="Background Image"
       />
-      <div
-        className={`backdrop-blur-sm bg-white-300/30 absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] rounded-lg border border-1 rgba(255, 255, 255, 0.5)`}
-      >
+      <div className="p-6 backdrop-blur-sm bg-white-300/30 absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] rounded-lg border border-1 rgba(255, 255, 255, 0.1)">
         <div className="relative flex flex-col justify-center items-center bg-gradient-radial">
           <div className=" flex flex-col justify-center items-center bg-gradient-radial">
-            {/* Background Image */}
+            {/* Project Description Section */}
             <div className="project-description-container mt-10">
-              {/* Project Description */}
               <h1 className="text-3xl text-white font-bold mb-4">
                 Aime-moi chez moi
               </h1>
-              <p>
+              <p className="w-[500px]">
                 Enter the name of your city to learn french while living your
                 own love story.
               </p>
             </div>
-            {/* Forms */}
-            {/* OpenAI Form and Button */}
-            <div className="flex flex-col items-center mr-8">
+            {/* Forms Section */}
+            <div className="flex flex-row space-x-4">
               {/* City input field */}
               <textarea
-                className="w-[500px] p-4 text-base font-normal border-2 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-pink-500 resize: none h-15 mb-4"
-                placeholder="Enter your city name and country."
+                className="w-[240px] text-base font-normal border-2 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-pink-500 resize-none mb-2"
+                placeholder="Enter your city."
                 value={userCity}
                 onChange={(e) => setUserCity(e.target.value)}
               />
-              {/* OpenAI output field */}
+              {/* Name input field */}
               <textarea
-                className="w-[500px] p-4 text-base font-normal border-2 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-pink-500 resize: none h-40 backdrop-blur-2xl"
-                placeholder="Somebody is coming to fall in love with you"
-                value={loveStory}
-                onChange={(e) => setloveStory(e.target.value)}
+                className="w-[240px] text-base font-normal border-2 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-pink-500 resize-none mb-2"
+                placeholder="Enter your name."
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
               />
-              <button
-                onClick={handleGenerateClick}
-                className="w-full px-4 py-2 bg-pink-500 text-white font-bold rounded hover:bg-pink-700 transition duration-300 mt-4"
-              >
-                Generate (OpenAI)
-              </button>
             </div>
+            <textarea
+              className="w-[500px] p-4 text-base font-normal border-2 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-pink-500 resize:none h-60 backdrop-blur-2xl"
+              placeholder="Somebody is coming to fall in love with you"
+              value={loveStory}
+              onChange={(e) => setloveStory(e.target.value)}
+            />
+            <button
+              onClick={handleGenerateClick}
+              className="w-[500px] px-4 py-2 bg-pink-500 text-white font-bold rounded hover:bg-pink-700 transition duration-300 mt-4"
+            >
+              Generate (OpenAI)
+            </button>
           </div>
-
           <div className="justify-center mt-auto mb-8 mt-8">
             <a
               href="https://frenchezleo.com"
