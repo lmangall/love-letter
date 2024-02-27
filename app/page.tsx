0c0d855d950c2c43fa6983b6121379e3058a8ca9
lmangall <l.mangallon@gmail.com>
2024-02-27 13:16:00 +0100

    non-editable output field

--- app/page.tsx
+++ app/page.tsx
@@ -19,9 +19,9 @@ export default function Home() {
         objectFit="cover"
         alt="Background Image"
       />
-      <div className="p-6 backdrop-blur-sm bg-white-300/30 absolute top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] rounded-lg border border-1 rgba(255, 255, 255, 0.1)">
-        <div className="relative flex flex-col justify-center items-center bg-gradient-radial">
-          <div className=" flex flex-col justify-center items-center bg-gradient-radial">
+      <div className="p-6 backdrop-blur-sm bg-white-300/30 fixed top-[10%] left-1/2 transform -translate-x-1/2 rounded-lg border border-1 rgba(255, 255, 255, 0.1) h-[80vh] overflow-hidden">
+        <div className="relative flex flex-col justify-center items-center bg-gradient-radial h-full">
+          <div className="flex flex-col justify-center items-center bg-gradient-radial overflow-y-auto w-full h-full">
             <div className="project-description-container mt-10">
               <h1 className="text-3xl text-white font-bold mb-4">
                 Aime-moi chez moi
@@ -43,12 +43,13 @@ export default function Home() {
                 onChange={(e) => setUserName(e.target.value)}
               />
             </div>
-            <textarea
-              className="bg-white bg-opacity-40 hover:bg-purple-100 hover:bg-opacity-20 w-[500px] p-4 text-base font-normal border-2 border-gray-300 rounded-lg shadow-sm focus:outline-none focus:border-pink-500 resize:none h-60 backdrop-blur-2xl"
-              placeholder="Somebody is coming to fall in love with you"
-              value={loveStory}
-              readOnly
-            />
+            <div
+              className="bg-white bg-opacity-40 hover:bg-purple-100 hover:bg-opacity-20 w-[500px] p-4 text-base font-normal border-2 border-gray-300 rounded-lg shadow-sm resize-none overflow-y-auto"
+              style={{ maxHeight: "50vh" }}
+              aria-readonly="true"
+            >
+              {loveStory || "Somebody is coming to fall in love with you..."}
+            </div>
             <Button onClick={() => fetchLoveStory(userCity, userName)}>
               Generate (OpenAI)
             </Button>
