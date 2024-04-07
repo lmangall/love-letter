c587129bc4a97c7cf6aa826aff80d2f4b3c49616
lmangall <l.mangallon@gmail.com>
2024-04-07 13:16:25 +0200

    basic modal added, typo

--- app/page.tsx
+++ app/page.tsx
@@ -329,7 +329,7 @@ export default function Home() {
               isOpen={isAboutModalOpen}
               onClose={() => setIsAboutModalOpen(false)}
             />
-            ;<button onClick={handleOpenAboutModal}>About</button>
+            <button onClick={handleOpenAboutModal}>About</button>
           </div>
         </div>
       </div>
